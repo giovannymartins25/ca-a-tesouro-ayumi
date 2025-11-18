12eeb1f6183aa6ff89c59c8a4465cab94db95849
Giovanny Gabryel Martins de Jesus <neusa2554@gmail.com>
2025-11-17 22:40:32 -0300

    1

--- pages/api/puzzles.js
+++ pages/api/puzzles.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const puzzles = [
     { id: 1, title: 'O primeiro mimo (De muitos)', clue: 'Qual a primeira coisa que eu comprei pra vc?', answer: 'temaki' },
-    { id: 2, title: 'A nosssa musica', clue: 'qual a "nossa música""? (a gente se assumiu com ela kk)', answer: 'Os anjos cantam' },
+    { id: 2, title: 'A nosssa musica', clue: 'qual a "nossa música"? (a gente se assumiu com ela kk)', answer: 'Os anjos cantam' },
     { id: 3, title: 'Hein?', clue: 'Você me ama?', answer: 'sim' }
   ]
   res.status(200).json({ puzzles })
